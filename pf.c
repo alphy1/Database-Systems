9cca10dc1a98d867e772de5860858c5413f61ab0
alphy1 <42415340+alphy1@users.noreply.github.com>
2020-04-22 09:10:47 +0300

    Update pf.c

--- pf.c
+++ pf.c
@@ -16,7 +16,7 @@ typedef struct PFhdr_str {
 typedef struct PFftab_ele {
     bool_t    valid;       /* set to TRUE when a file is open. */
     ino_t     inode;       /* inode number of the file         */
-    char      *fname;      /* file name                        */
+    const char      *fname;      /* file name                        */
     int       unixfd;      /* Unix file descriptor             */
     PFhdr_str hdr;         /* file header                      */
     short     hdrchanged;  /* TRUE if file header has changed  */
