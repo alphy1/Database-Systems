63e08ddb0c2e38678c5bc99a238b94c96d48c9d9
joonssl <63773450+joonssl@users.noreply.github.com>
2020-05-21 21:42:34 +0900

    Update am.c

--- am.c
+++ am.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <string.h>
+#include "am.h"
+#include "minirel.h"
+#include "bf.h"
+#include "pf.h"
 
-
-typedef  struct _hf_record_identification {
-	int	pagenum;
-	int	recnum;
-} RECID;
-
-#define PAGE_SIZE		4096
 #define ORDER 8
-#define AM_ITAB_SIZE 20
-typedef enum { FALSE = 0, TRUE } bool_t;
 
 struct B_node;
 
@@ -39,26 +33,157 @@ typedef struct {
 RECID rootarry[AM_ITAB_SIZE];
 AM_header header[AM_ITAB_SIZE];
 int order = ORDER;
-B_node* m[10000];
-int a = 0;
+int AM_SAVE_ERROR(int error){
+    AMerrno = error;
+    return error;
+}
+
+
+#define unpinPage(fd, nodeId, dirty)  PF_UnpinPage(fd, nodeId.pagenum, dirty)
+int ldpn[10000];
+char * ldp[10000];
+int cntforld;
+int AMerrno;
+char *findld(pn)
+{
+	int i;
+	for(i=0;i<cntforld;i++)
+		if(ldpn[i]==pn)break;
+
+	if(i==cntforld)
+		return NULL;
+	
+	return ldp[i];
+	
+}
+
+void setld(int pn,char *pb)
+{
+		ldpn[cntforld] = pn;
+		ldp[cntforld] = pb;
+		cntforld++;
+}
+
+void unpinAll(int fd, int dirty)
+{
+    
+	int i;
+	for(i=0;i<cntforld;i++)
+        PF_UnpinPage(fd, ldpn[i], dirty);
+
+	
+	cntforld = 0;
+}
+
 
 B_node *get_Bnode(int fd, RECID nodeId) {
-  return m[nodeId.pagenum];
+	if(nodeId.recnum == -1)return NULL;
+		
+	char *pagebuf = findld(nodeId.pagenum);
+
+	if(pagebuf == NULL)
+	{
+
+		if(PF_GetThisPage(fd, nodeId.pagenum, &pagebuf) != PFE_OK)
+			return NULL;
+		
+		setld(nodeId.pagenum, pagebuf);
+
+	}
+
+	pagebuf += nodeId.recnum;
+	return (B_node *)pagebuf;
 }
 
 B_node *allocate_Bnode(int fd) {
-	int offset = sizeof(B_node) + ORDER*header[fd].length;
 
-	m[a]=(B_node*)malloc(offset);
-	B_node node={.is_leaf=0,.num_keys=0,.id={a,0}};
-    
-	memcpy(m[a], &node, sizeof(B_node));
-    //printf("AAAA %d\n", m[a]->id.pagenum);
-	return m[a++];
+	int pagenum, rtn;
+	char *pagebuf;
+	if(PF_GetFirstPage(fd, &pagenum, &pagebuf) != PFE_OK)
+		return NULL;
+
+    if(PF_UnpinPage(fd, pagenum, FALSE) != PFE_OK)
+		return NULL;
+
+	while(1)
+	{
+
+		rtn = PF_GetNextPage(fd, &pagenum, &pagebuf);
+
+		if(rtn!=PFE_OK)break;
+		int offset = *(int*)(pagebuf + sizeof(int));
+		int nextoffset = offset + sizeof(B_node) + ORDER*header[fd].length;
+        
+      
+		if(nextoffset < PAGE_SIZE)
+		{
+				if(findld(pagenum)==NULL)    
+					setld(pagenum, pagebuf);
+				else 
+					if(PF_UnpinPage(fd, pagenum, FALSE) != PFE_OK)
+						return NULL;
+
+			  int cnt = *(int*)pagebuf;
+			  cnt++;
+			  memcpy(pagebuf, &cnt, sizeof(int));
+            memcpy(pagebuf+sizeof(int), &nextoffset, sizeof(int));
+                
+				B_node node={.is_leaf=0,.num_keys=0,.id={pagenum,offset}};
+				pagebuf+=offset;
+			  memcpy(pagebuf, &node, sizeof(B_node));
+			  
+
+			 	
+				return (B_node *)(pagebuf);
+		}
+		if(PF_UnpinPage(fd, pagenum, FALSE) != PFE_OK)
+			return NULL;
+	}
+	
+	if(rtn == PFE_EOF) 
+	{
+		if(PF_AllocPage(fd, &pagenum, &pagebuf) != PFE_OK)
+			return NULL; 
+
+		setld(pagenum, pagebuf);
+		int cnt = 1, offset = 2*sizeof(int) + sizeof(B_node) + ORDER*header[fd].length;
+		B_node node={.is_leaf=0,.num_keys=0,.id={pagenum,2*sizeof(int)}};
+
+		memcpy(pagebuf, &cnt, sizeof(int));
+		memcpy(pagebuf+sizeof(int), &offset, sizeof(int));
+		memcpy(pagebuf+2*sizeof(int), &node, sizeof(B_node));
+        
+		return (B_node *)(pagebuf + 2*sizeof(int));
+	}
+	else return NULL;
 }
 
-void delete_Bnode(int fd, RECID nodeId) {
 
+int delete_Bnode(int fd, RECID nodeId) {
+	
+    
+    
+	char *pagebuf = findld(nodeId.pagenum);	
+    
+	if(pagebuf == NULL)
+	{
+		if(PF_GetThisPage(fd, nodeId.pagenum, &pagebuf) != PFE_OK)
+			return NULL;
+
+		setld(nodeId.pagenum, pagebuf);
+	}
+	
+	int cnt = *(int*)pagebuf;
+    if(cnt==0)return NULL;
+    
+	cnt--;
+	memcpy(pagebuf, &cnt, sizeof(int));
+	
+	int offset = 2*sizeof(int);
+	if(cnt == 0)
+		 memcpy(pagebuf+sizeof(int), &offset, sizeof(int));
+
+	return 1; 
 }
 
 char *get_key(int fd, RECID nodeId, int index) {
@@ -264,24 +389,28 @@ int insert_into_parent(int fd, B_node *root, B_node *left, B_node *right, char *
 
 B_node *find_leaf(int fd, B_node *root, char *key) {
 	B_node *c = root;
-	while (!c->is_leaf) {
+    
+	while (!c->is_leaf) {       
         short int flag = 1;
 		for (int i = 0; i < c->num_keys; i++) {
-		    //printf("%d %d\n", *(int *) key, *(int *) get_key(fd, c->id, i));
 			if (compare(fd, key, get_key(fd, c->id, i)) < 0) {
                 c = (B_node *)get_Bnode(fd,c->children[i]);
                 flag = 0;
                 break;
 			}
 		}	
+
 		if (flag)
             c = (B_node*)get_Bnode(fd,c->children[c->num_keys]);
+     
 	}
 	return c;
 }
 
 int insert(int fd, B_node *root, char *key, RECID recId) {
+   
 	B_node *leaf = find_leaf(fd, root, key);
+
 	if (leaf->num_keys < order - 1) {
         int insertInto = 0;
         while (insertInto < leaf->num_keys && compare(fd, get_key(fd, leaf->id, insertInto), key) < 0)
@@ -546,51 +675,320 @@ int delete(int fd, B_node *root, char *key) {
 	return -12;
 }
 
-int main()
-{
-	header[0].length = 4;
-    header[0].type = 'i';
-	B_node *root = make_leaf(0);
-    rootarry[0] = root->id;
-	RECID recId = {0,1};
-	int n = 100;
-    for(int i = 0; i < n; ++i)
-    {
-        root = get_Bnode(0, rootarry[0]);
-        //printf("ROOT %d\n", *(int *)get_key(0, root->id, 0));
-	    insert(0, root, (char *)&i, recId);
-    
-    }
-    //printf("%d\n", root->id.pagenum );
-    //printf("ROOT %d\n", *(int *)get_key(0, root->id, 0));
+
+
+
+void AM_Init() {
+	HF_Init();
+}
+
+int  AM_CreateIndex	(const char *fileName, int indexNo, char attrType, int attrLength, bool_t isUnique) {
 	
-	B_node *node = get_first_Bnode(0, get_Bnode(0, rootarry[0]));
+	if(attrType == INT_TYPE||attrType == REAL_TYPE)
+	{
+		if(attrLength != 4)
+			return AM_SAVE_ERROR(AME_INVALIDATTRLENGTH);
+	}
+	else if(attrType == STRING_TYPE)
+    {
+		if(attrLength < 1 || attrLength > 255)
+			return AM_SAVE_ERROR(AME_INVALIDATTRLENGTH);
+    }
+	else return AM_SAVE_ERROR(AME_INVALIDATTRTYPE);
+	
+	char name [255];
+    snprintf ( name, 255, "%s.%d", fileName, indexNo);
+    
+	if(PF_CreateFile(name) != PFE_OK)
+		return AM_SAVE_ERROR(AME_PF);
+	      
+
+    
+	int fd = PF_OpenFile(name);
+
+    if(fd < 0)
+    {
+        if(fd == PFE_FTABFULL)
+            return AM_SAVE_ERROR(AME_FULLTABLE);
+        else return AM_SAVE_ERROR(AME_PF);
+    }
+
+    
+	int pagenum, pagenum1;
+	char *pagebuf,*pagebuf1;
+
+
+	if(PF_AllocPage(fd, &pagenum, &pagebuf) != PFE_OK)
+		return AM_SAVE_ERROR(AME_PF);   
+
+	if(PF_AllocPage(fd, &pagenum1, &pagebuf1) != PFE_OK)
+		return AM_SAVE_ERROR(AME_PF);   
+		
+    
+ 
+	int cnt = 0, offset = 2*sizeof(int);
+	memcpy(pagebuf1, &cnt, sizeof(int));
+	memcpy(pagebuf1+sizeof(int), &offset, sizeof(int));
+	
+	if(PF_UnpinPage(fd, pagenum1, TRUE) != PFE_OK)
+		return AM_SAVE_ERROR(AME_PF);
+    
+    header[fd].type = attrType;
+    header[fd].length = attrLength;
+
+	B_node *root=make_leaf(fd);
+	
+	header[fd].rootId=root->id;
+			      
+	memcpy(pagebuf, &header[fd], sizeof(AM_header));
+	if(PF_UnpinPage(fd, pagenum, TRUE) != PFE_OK)
+		return AM_SAVE_ERROR(AME_PF);       
+    
+    unpinAll(fd, TRUE);
+	if(PF_CloseFile(fd) != PFE_OK)
+		return AM_SAVE_ERROR(AME_PF);
+     
+	return AME_OK;
+}
+
+int  AM_DestroyIndex	(const char *fileName, int indexNo) {
+
+	char name [255];
+  snprintf ( name, 255, "%s.%d", fileName, indexNo);
+  
+	if(PF_DestroyFile(name) != PFE_OK)
+		return AM_SAVE_ERROR(AME_PF);
+
+	return AME_OK;
+
+}
+
+int  AM_OpenIndex       (const char *fileName, int indexNo) {
+	char name [255];
+  snprintf ( name, 255, "%s.%d", fileName, indexNo);
+  
+  int fd = PF_OpenFile(name);
+  if(fd < 0)
+  {
+	if(fd == PFE_FTABFULL)
+		return AM_SAVE_ERROR(AME_FULLTABLE);
+	else return AM_SAVE_ERROR(AME_PF);
+  }
+	char *pagebuf;
+
+	if(PF_GetThisPage(fd, 0, &pagebuf) != PFE_OK)
+		return AM_SAVE_ERROR(AME_PF);
+
+	header[fd] = *(AM_header*)pagebuf;
+	rootarry[fd] = header[fd].rootId;
+	
+	if(PF_UnpinPage(fd, 0, FALSE) != PFE_OK)
+		return AM_SAVE_ERROR(AME_PF);
+
+	return fd;
+}
+
+int  AM_CloseIndex      (int fd) {
+	
+	char *pagebuf;
+	if(PF_GetThisPage(fd, 0, &pagebuf) != PFE_OK)
+		return AM_SAVE_ERROR(AME_PF);
+		
+	AM_header hd={header[fd].type, header[fd].length, header[fd].rootId};
+	memcpy(pagebuf, &hd, sizeof(AM_header));
+	
+	if(PF_UnpinPage(fd, 0, TRUE) != PFE_OK)
+		return AM_SAVE_ERROR(AME_PF);
+		
+	if(PF_CloseFile(fd) != PFE_OK)
+		return AM_SAVE_ERROR(AME_PF);
+}
+
+int  AM_InsertEntry	(int fileDesc, char *value, RECID recId) {
+
+	B_node * root = get_Bnode(fileDesc, rootarry[fileDesc]);
+
+	if(root==NULL)return AM_SAVE_ERROR(AME_FD);
+			
+	int rtn = insert(fileDesc, root, value, recId);
+	unpinAll(fileDesc, TRUE);
+	if(rtn==1)rtn = AME_OK;
+	return AM_SAVE_ERROR(rtn);
+}
+
+int  AM_DeleteEntry(int fileDesc, char *value, RECID recId) {
+	B_node * root = get_Bnode(fileDesc, rootarry[fileDesc]);
+	if(root==NULL)return AM_SAVE_ERROR(AME_FD);
+
+	int rtn = delete(fileDesc, root, value);
+	if(rtn==1)rtn = AME_OK;
+	unpinAll(fileDesc, TRUE);
+	return AM_SAVE_ERROR(rtn);;
+}
+
+
+typedef struct{
+    int opened;
+		int fileDesc;
+    int op;
+    entry curr;
+    entry first;
+    entry last;
+}AMScan;
+
+AMScan Scan_Table[MAXISCANS];
+
+int  AM_OpenIndexScan	(int fileDesc, int op, char *value) {
+	RECID recId = {0,1};
+	int n = 43 ;
+    
+    	B_node *node = get_first_Bnode(fileDesc, get_Bnode(fileDesc, rootarry[fileDesc]));
 	entry e = {node -> id, 0};
-	//	 printf("%d  ", *(int*)get_key(0, root->id, 0));
+		 printf("%d  ", *(int*)get_key(fileDesc, rootarry[fileDesc], 0));
 	for (int i = 0; i < n - 1; ++i) {
-    //	printf("%d\n", *(int *)get_key(0, e.nodeId, e.index));
-	    get_next_entry(0, &e);
+    	printf("%s\n", get_key(fileDesc, e.nodeId, e.index));
+	    get_next_entry(fileDesc, &e);
 	}
-	entry first = {-1, -1};
-	entry last = {-1, -1};
-	int x = 100;
-	insert(0, get_Bnode(0, rootarry[0]), (char *)&x, recId);
-	find_equal(0, get_Bnode(0, rootarry[0]), (char *)&x, &first, &last);
-	printf("%d\n", *(int *)get_key(0, first.nodeId, first.index));
-	/*get_next_entry(0, &first);
-	printf("%d\n", *(int *)get_key(0, first.nodeId, first.index));
-	*/
-	int d = 100;
-	delete(0, get_Bnode(0, rootarry[0]), (char *)&d);
-	find_gequal(0,get_Bnode(0, rootarry[0]), (char *)&x, &first, &last);
-	printf("%d %d %d %d\n", first.nodeId.pagenum, first.index, last.nodeId.pagenum, last.index);
-	while (first.nodeId.pagenum <= last.nodeId.pagenum) {
-	    //printf("%d %d %d %d\n", first.nodeId.pagenum, first.index, last.nodeId.pagenum, last.index);
-	    printf("%d\n", *(int *)get_key(0, first.nodeId, first.index));
-	    if (first.nodeId.pagenum == last.nodeId.pagenum && first.index >= last.index - 1)
-	        break;
-	    get_next_entry(0, &first);
-	    //printf("%d %d %d %d\n", first.nodeId.pagenum, first.index, last.nodeId.pagenum, last.index);
+
+    
+	if(value == NULL)
+		op = ALL_OP;
+	
+	int i;
+   	/* B_node *a;
+        RECID aa;
+        int t = 8;
+            
+        for(i=1; i<11;i++)
+    {
+        for(int j=1; j<8;j++)
+        {        
+         aa.pagenum = i;
+         aa.recnum = t;
+         t += 360;
+         if(t > 2168)t=8;
+         a = get_Bnode(fileDesc,aa);  
+         printf("qqcbbb  %d  %d  %d\n",i,t,a->id.recnum);  
+         printf("cbbb  %d  %d  %d\n",i,a->id.pagenum,a->id.recnum);  
+        }
+    }
+    */
+    
+	for(i=0;i<MAXISCANS;i++)
+		if(Scan_Table[i].opened == 0)
+			break;
+			
+	if(i==MAXISCANS)
+		return AM_SAVE_ERROR(AME_SCANTABLEFULL);
+		
+	Scan_Table[i].opened = 1;
+	Scan_Table[i].fileDesc = fileDesc;
+	Scan_Table[i].op = op;
+	
+	B_node * root = get_Bnode(fileDesc,  rootarry[fileDesc]);
+	if(root==NULL)return AM_SAVE_ERROR(AME_FD);
+		
+	switch(op){
+	 	case EQ_OP :
+			find_equal(fileDesc, root,  value, &Scan_Table[i].first, &Scan_Table[i].last);
+			Scan_Table[i].curr = Scan_Table[i].first;
+	 		break;
+	 	case LT_OP :
+			find_less(fileDesc, root,  value, &Scan_Table[i].first, &Scan_Table[i].last);
+			Scan_Table[i].curr = Scan_Table[i].first;
+	 		break;
+	 	case GT_OP :
+			find_greater(fileDesc, root,  value, &Scan_Table[i].first, &Scan_Table[i].last);
+			Scan_Table[i].curr = Scan_Table[i].first;
+            //printf("
+	 		break;
+	 	case LE_OP :
+			find_lequal(fileDesc, root,  value, &Scan_Table[i].first, &Scan_Table[i].last);
+			Scan_Table[i].curr = Scan_Table[i].first;
+	 		break;
+	 	case GE_OP :
+			find_gequal(fileDesc, root,  value, &Scan_Table[i].first, &Scan_Table[i].last);
+			Scan_Table[i].curr = Scan_Table[i].first;
+	 		break;
+	 	case NE_OP :
+	 		find_equal(fileDesc, root,  value, &Scan_Table[i].first, &Scan_Table[i].last);
+	 		Scan_Table[i].curr = Scan_Table[i].first;
+	 		break;
+	 	case ALL_OP :
+	 		Scan_Table[i].first.nodeId = get_first_Bnode(fileDesc, root)->id;
+	 		Scan_Table[i].first.index = 0;
+	 		
+	 		B_node *lst = get_last_Bnode(fileDesc, root);
+	 		Scan_Table[i].last.nodeId = lst->id;
+	 		Scan_Table[i].last.index = lst->num_keys;
+	 		
+	 		Scan_Table[i].curr = Scan_Table[i].first;
+	 		
+
+	 		break;
+	 	default :
+	 		return AM_SAVE_ERROR(AME_INVALIDOP);
+	 }
+	 
+	if(Scan_Table[i].first.nodeId.pagenum == Scan_Table[i].last.nodeId.pagenum 
+    && Scan_Table[i].first.nodeId.recnum == Scan_Table[i].last.nodeId.recnum 
+    && Scan_Table[i].first.index==Scan_Table[i].last.index) 
+	{
+		Scan_Table[i].curr.index = -1;
+
+	}	 
+	if(Scan_Table[i].curr.index == -1) return AM_SAVE_ERROR(AME_RECNOTFOUND);
+    
+	 unpinAll(fileDesc, FALSE);
+	 return i;
+}
+
+RECID AM_FindNextEntry	(int scanDesc) {
+	
+    RECID invalrecid = {-1,-1};
+	if(scanDesc > MAXISCANS || Scan_Table[scanDesc].opened != 1)
+    {
+		AM_SAVE_ERROR(AME_INVALIDSCANDESC);
+        return invalrecid;
+    }
+    
+	AMScan sc = Scan_Table[scanDesc];
+
+    if(sc.curr.index = -1)
+    {
+		AM_SAVE_ERROR(AME_EOF);
+        return invalrecid;
+    }
+			
+
+	B_node * node = get_Bnode(sc.fileDesc,  sc.curr.nodeId);
+	RECID recid =  node->children[sc.curr.index+1];
+	
+	if (sc.curr.index + 1 < node->num_keys) 
+		Scan_Table[scanDesc].curr.index++;
+	else
+	{
+		Scan_Table[scanDesc].curr.nodeId = get_Bnode(sc.fileDesc, node->children[node->num_keys + 1])->id;
+		Scan_Table[scanDesc].curr.index=0;
 	}
-	return 0;
+	
+    if(sc.curr.nodeId.pagenum == sc.last.nodeId.pagenum 
+    && sc.curr.nodeId.recnum == sc.last.nodeId.recnum 
+    && sc.curr.index==sc.last.index) 
+		Scan_Table[scanDesc].curr.index=-1;
+        
+	unpinAll(sc.fileDesc, FALSE);
+	return recid;
+}
+
+int  AM_CloseIndexScan	(int scanDesc) {
+	if(scanDesc > MAXISCANS || Scan_Table[scanDesc].opened != 1)
+		 return AM_SAVE_ERROR(AME_INVALIDSCANDESC);
+	Scan_Table[scanDesc].opened = 0;
+	return AME_OK;
+}
+
+void AM_PrintError	(const char *errString) {
+    fprintf(stderr, "%s\n", errString);
+    fprintf(stderr, "%d\n", AMerrno);
 }
