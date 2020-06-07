bcc3de44461dfd70cc8bdbb2b2b5eec000b4bf36
Kerim Kochekov <50640842+KerimKochekov@users.noreply.github.com>
2020-06-07 18:36:31 +0300

    Update fe.c

--- fe.c
+++ fe.c
@@ -21,10 +21,10 @@ ATTR_DESCR in_attrs[RELCAT_NUM_ATTRS];
 ATTR_DESCR attributes[MAXN];
 int FEerrno,initialized; //FE layer error code
 void make_assign(ATTR_DESCR *attr, const char *name, char type, int len){
-  attr->attrName = (char *) malloc(MAXNAME);
-  strcpy(attr->attrName, name);
-  attr->attrType = type;
-  attr->attrLen = len;
+	attr->attrName = (char *) malloc(MAXNAME);
+	strcpy(attr->attrName, name);
+	attr->attrType = type;
+	attr->attrLen = len;
 }
 int FE_SAVE_ERROR(int error){
 	return FEerrno = error;
@@ -38,7 +38,6 @@ typedef struct _relation_desc {
 	struct _relation_desc *next;
 } RELDESCTYPE;
 RELDESCTYPE *rel_head;
-
 typedef struct _attribute_desc {
     char relname[MAXNAME];	/* relation name			*/
     char attrname[MAXNAME];	/* attribute name			*/
@@ -50,8 +49,29 @@ typedef struct _attribute_desc {
 	struct _attribute_desc *next; //Next of Single-Linked-List
 } ATTRDESCTYPE;
 ATTRDESCTYPE *attr_head;
-
+int equal_rel_name(char *x,const char *y){
+	int xx = strlen(x),yy = strlen(y);
+	if(xx != yy)
+		return 0;
+	for(int i = 0; i < xx; i++)
+		if(x[i] != y[i])
+			return 0;
+	return 1;
+}
 int CreateTable(const char *relName,int numAttrs,ATTR_DESCR attrs[],const char *primAttrName){
+	if(strlen(relName) > MAXNAME)
+		return FE_SAVE_ERROR(FEE_RELNAMETOOLONG);
+	for(int i = 0; i < numAttrs; i++){
+		if(strlen(attrs[i].attrName) > MAXNAME)
+			return FE_SAVE_ERROR(FEE_ATTRNAMETOOLONG);
+		if(attrs[i].attrType != INT_TYPE && attrs[i].attrType != STRING_TYPE && 
+			attrs[i].attrType != REAL_TYPE && attrs[i].attrType != BOOL_TYPE)
+			return FE_SAVE_ERROR(FEE_INVATTRTYPE);
+	}
+	for(int i = 0; i < numAttrs; i++)	
+		for(int j = i + 1; j < numAttrs; j++)
+			if(equal_rel_name(attrs[i].attrName,attrs[j].attrName))
+				return FE_SAVE_ERROR(FEE_DUPLATTR);
 	int offset = 0, pos = 0;
 	if(attr_head == NULL){
 		if(!numAttrs)
@@ -90,24 +110,20 @@ int CreateTable(const char *relName,int numAttrs,ATTR_DESCR attrs[],const char *
 			rel_node = rel_node->next;
 		rel_node->next = tmp;
 	}
-	HF_CreateFile(relName,offset);
+	if(HF_CreateFile(relName,offset) != 0)
+		return FE_SAVE_ERROR(FEE_HF);
 	return FEE_OK;
 }
-int equal_rel_name(char *x,const char *y){
-	int xx = strlen(x),yy = strlen(y);
-	if(xx != yy)
-		return 0;
-	for(int i = 0; i < xx; i++)
-		if(x[i] != y[i])
-			return 0;
-	return 1;
-}
 int DestroyTable(const char *relName){
+	if(strlen(relName) > MAXNAME)
+		return FE_SAVE_ERROR(FEE_RELNAMETOOLONG);
+	//Deleting relation from attrcat catalog
 	while(attr_head != NULL && equal_rel_name(attr_head->relname,relName))
 		attr_head = attr_head->next;
 	ATTRDESCTYPE *attr_node = attr_head, *pre;
 	while(attr_node != NULL){
 		if(equal_rel_name(attr_node->relname,relName)){
+			//AM_DestroyIndex(relName,attr_node->attrno);
 			if(pre != NULL){
 				pre->next = attr_node->next;
 				attr_node = pre->next;
@@ -120,7 +136,7 @@ int DestroyTable(const char *relName){
 			attr_node = attr_node->next;
 		}
 	}
-	
+	//Deleting relation from relcat catalog
 	if(rel_head != NULL && equal_rel_name(rel_head->relname,relName))
 		rel_head = rel_head->next;
 	else{
@@ -136,18 +152,27 @@ int DestroyTable(const char *relName){
 			}
 		}
 	}
-	HF_DestroyFile(relName);
+	if(HF_DestroyFile(relName) != 0)
+		return FE_SAVE_ERROR(FEE_HF);
 	return FEE_OK;
 }
 int BuildIndex(const char *relName,	const char *attrName){
+	if(strlen(relName) > MAXNAME)
+		return FE_SAVE_ERROR(FEE_RELNAMETOOLONG);
+	if(strlen(attrName) > MAXNAME)
+		return FE_SAVE_ERROR(FEE_ATTRNAMETOOLONG);
 	ATTRDESCTYPE *attr_node = attr_head;
 	while(attr_node != NULL){
 		if(equal_rel_name(attr_node->relname,relName) && equal_rel_name(attr_node->attrname,attrName)){
-			//AM_CreateIndex(relName,attr_node->attrno,attr_node->attrtype,attr_node->attrlen,FALSE);
+			if(attr_node->indexed == TRUE)
+				return FE_SAVE_ERROR(FEE_ALREADYINDEXED);
 			attr_node->indexed = TRUE;
+			//AM_CreateIndex(relName,attr_node->attrno,attr_node->attrtype,attr_node->attrlen,FALSE);
 			break;
 		}
 		attr_node = attr_node->next;
+		if(attr_node == NULL)
+			return FE_SAVE_ERROR(FEE_NOSUCHATTR);
 	}
 	RELDESCTYPE *rel_node = rel_head;
 	while(rel_node != NULL){
@@ -156,16 +181,21 @@ int BuildIndex(const char *relName,	const char *attrName){
 			break;
 		}
 		rel_node = rel_node->next;
+		if(rel_node == NULL)
+			return FE_SAVE_ERROR(FEE_NOSUCHREL);
 	}
 	return FEE_OK;
 }
 int DropIndex(const char *relname, const char *attrName){
+	if(strlen(relname) > MAXNAME)
+		return FE_SAVE_ERROR(FEE_RELNAMETOOLONG);
+	if(strlen(attrName) > MAXNAME)
+		return FE_SAVE_ERROR(FEE_ATTRNAMETOOLONG);
 	ATTRDESCTYPE *attr_node = attr_head;
 	while(attr_node != NULL){
 		if(equal_rel_name(attr_node->relname,relname) && (attrName == NULL || equal_rel_name(attr_node->attrname,attrName))){
-			//AM_CreateIndex(relname,attr_node->attrno,attr_node->attrtype,attr_node->attrlen,FALSE);
+			//AM_DestroyIndex(relname,attr_node->attrno);
 			attr_node->indexed = FALSE;
-			break;
 		}
 		attr_node = attr_node->next;
 	}
@@ -176,10 +206,12 @@ int DropIndex(const char *relname, const char *attrName){
 			break;
 		}
 		rel_node = rel_node->next;
+		if(rel_node == NULL)
+			return FE_SAVE_ERROR(FEE_NOSUCHREL);
 	}
 	return FEE_OK;
 }
-void relax(int x){
+void relax(int x){//Printing the frame of table
 	for(int i = 0; i < 15*x+1; i++){
 		if(i%15 == 0)
 			printf("+");
@@ -188,6 +220,8 @@ void relax(int x){
 	}puts("");
 }
 int PrintTable(const char *relName){
+	if(strlen(relName) > MAXNAME)
+		return FE_SAVE_ERROR(FEE_RELNAMETOOLONG);
 	printf("Relation %s:\n",relName);
 	if(equal_rel_name("relcat",relName)){
 		relax(5);
@@ -270,13 +304,16 @@ int PrintTable(const char *relName){
 			recid = HF_GetNextRec(fd,recid,record);
 		}
 		recid = HF_GetFirstRec(fd,record);
-		if(HF_ValidRecId(fd,recid))
+		if(HF_ValidRecId(fd,recid)) //If there is at lest one tuple, then in the end draw ending frame
 			relax(cnt_attribute);
-		HF_CloseFile(fd);
+		if(HF_CloseFile(fd) != 0)
+			return FE_SAVE_ERROR(FEE_HF);
 	}
 	return FEE_OK;
 }
 int LoadTable(const char *relName, const char *fileName){
+	if(strlen(relName) > MAXNAME)
+		return FE_SAVE_ERROR(FEE_RELNAMETOOLONG);
 	RELDESCTYPE *rel_node = rel_head;
 	int record_size = 0;
 	while(rel_node != NULL){
@@ -288,13 +325,18 @@ int LoadTable(const char *relName, const char *fileName){
 	}
 	int fd = HF_OpenFile(relName);
 	int unixfd = open(fileName, O_RDWR|O_SYNC);
+	if(unixfd < 0)
+		return FE_SAVE_ERROR(FEE_UNIX);
 	char record[PAGE_SIZE];
     while(read(unixfd, &record, record_size) == record_size)
 		HF_InsertRec(fd,record);
-	HF_CloseFile(fd);
+	if(HF_CloseFile(fd) != 0)
+		return FE_SAVE_ERROR(FEE_HF);
 	return FEE_OK;
 }
 int HelpTable(const char *relName){
+	if(strlen(relName) > MAXNAME)
+		return FE_SAVE_ERROR(FEE_RELNAMETOOLONG);
 	RELDESCTYPE *rel_node = rel_head;
 	while(rel_node != NULL){
 		if(equal_rel_name(rel_node->relname,relName))	
@@ -345,7 +387,8 @@ char FE_Error_Names[FE_NERRORS][100]={"FE OK","Alredy indexed","Attribute name t
 									"Invalid Operation"};
 void FE_PrintError(const char *errString){
     fprintf(stderr, "%s\n", errString);
-    fprintf(stderr, "%s\n", FE_Error_Names[-FEerrno]);
+	if(-FEerrno < FE_NERRORS)
+    	fprintf(stderr, "%s\n", FE_Error_Names[-FEerrno]);
 }
 void FE_Init(void){
 	HF_Init();
@@ -353,16 +396,25 @@ void FE_Init(void){
 }
 void DBdestroy(const char *dbname){
 	char command[MAXN];
-	sprintf(command,"rmdir %s\n",dbname);
+	sprintf(command,"rm -r %s\n",dbname);
 	system(command);
 }
 void DBconnect(const char *dbname){
 	chdir(dbname);
+	
 	FILE *fp = fopen("relcat","r");
+	if(fp == NULL){
+		FE_SAVE_ERROR(FEE_RELCAT);
+		return;
+	}
 	fscanf(fp,"%p",&rel_head);
 	fclose(fp);
 
 	fp = fopen("attrcat","r");
+	if(fp == NULL){
+		FE_SAVE_ERROR(FEE_CATALOGCHANGE);
+		return;
+	}
 	fscanf(fp,"%p",&attr_head);
 	fclose(fp);
 }
@@ -380,6 +432,7 @@ void DBclose(const char *dbname){
 void DBcreate(const char *dbname){
 	if(!initialized)
 		FE_Init(), initialized = 1;
+	//Creating the directory
 	char command[MAXN];
 	sprintf(command,"mkdir %s\n",dbname);
 	system(command);
